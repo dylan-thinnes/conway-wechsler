b6af0cd46d24d55ec1c198b9c56f611bd154fe8e
dylan-thinnes <dylan.thinnes@protonmail.com>
2019-01-20 08:29:41 +0000

    Add convenience functions for combining Fields

--- conway-wechsler.hs
+++ conway-wechsler.hs
@@ -1,5 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
 import qualified Data.Sequence as S
 import qualified Data.Text     as T
+import Control.Monad (mzero)
+import Data.Foldable (toList)
 
 -- Data type for expressing results of parsing
 type Fields = S.Seq T.Text
+
+-- | Extract the concatenation of a group of fields
+extract :: Fields -> T.Text
+extract = T.concat . toList . collapse
+
+-- | Run a function on a group of fields if the fields are not empty
+runIfNotNull :: (Fields -> Fields) -> Fields -> Fields
+runIfNotNull func fields = if fields == mzero
+                           then fields
+                           else func fields
+
+-- | Collapse a group of fields into a single field
+collapse :: Fields -> Fields
+collapse = runIfNotNull (pure . T.concat . toList)
