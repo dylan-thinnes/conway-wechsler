a00542d6a95866138de141dca1dfb4b381bed923
dylan-thinnes <dylan.thinnes@protonmail.com>
2019-01-20 08:33:55 +0000

    Add Triple type for grouping numbers by threes

--- conway-wechsler.hs
+++ conway-wechsler.hs
@@ -28,3 +28,18 @@ wordify = S.intersperse " "
 -- | Put newlines between each parsed result
 lineify :: Fields -> Fields
 lineify = S.intersperse "\n"
+
+-- | Data type for storing & unstoring groups of numbers by three
+-- This will help in inflecting both normal numbers and prefixes for zillions
+data Triple = Triple {
+                hun :: Int,
+                ten :: Int,
+                one :: Int
+              }
+    deriving (Show,Eq)
+
+intsToTriple :: S.Seq Int -> Triple
+intsToTriple s = Triple (S.index s 0) (S.index s 1) (S.index s 2)
+
+tripleToInt :: Integral a => Triple -> a
+tripleToInt (Triple h t o) = fromIntegral $ h * 100 + t * 10 + o
