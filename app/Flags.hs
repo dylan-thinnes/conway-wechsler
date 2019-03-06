2672cbc1aa72e9765ec2233b1d57b674cb4e9e67
Dylan Thinnes <dylan.thinnes@protonmail.com>
2019-03-06 21:50:33 +0000

    Convert tryGetInput to only return strings

--- app/Flags.hs
+++ app/Flags.hs
@@ -96,12 +96,12 @@ multipleInputFlagsError fs = Left
     showInput _         = error "Non-input flag supplied to showInput"
 
 -- | Try and turn one of two input flags, Stdin or Input n, into a meaningful
--- Integer value
+-- String value
 -- * Stdin means try read from stdin
--- * Input i means just return i
-tryGetInput :: Flag -> IO (Either String Integer)
-tryGetInput Stdin     = (Input <$> getLine) >>= tryGetInput
-tryGetInput (Input s) = return $ tryExprToInt True =<< tryStrToExpr s
+-- * Input s means just return s
+tryGetInput :: Flag -> IO (Either String String)
+tryGetInput Stdin     = getLine
+tryGetInput (Input s) = pure s
 
 -- | Try to turn a string into an expression
 tryStrToExpr :: String -> Either String MP.Expr
